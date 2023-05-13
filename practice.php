13fc33cfd502654f29268931e339afff89577c3e
Akash A <akash.a.dckap@gmail.com>
2023-05-13 17:58:01 +0530

    ArrayAsendingOrder

--- practice.php
+++ practice.php
@@ -60,38 +60,3 @@ print_r($results);
 
 
 
-//Index accessing method
-// $input = [1,2,3];
-// $results = [];
-// for($i=0; $i<count($input)-1; $i++){
-//     // print_r($input[2]);
-//     if($input[$i] < $input[1]){
-//         // $results[] =($input[$i]);
-//         $a = $input[$i];
-//         $b =  $input[1];
-        
-//         $input[$i] = $b;
-//         $input[1] = $a;
-//         array_push($results,$input);
-//     }
-//     if($input[$i] < $input[2]){
-//         // $results[] =($input[$i]);
-//         $a = $input[$i];
-//         $b =  $input[2];
-        
-//         $input[$i] = $b;
-//         $input[2] = $a;
-//         array_push($results,$input);
-//     }
-
-//     else{
-//         array_push($results,$input);
-//     }   
-    
-// }
-// print_r($results);
-// print_r($results);
-
-// else{
-//     print_r($results);
-// }    
